c60c28e0d68c1dd8625e8ea86e5d768bb269f671
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-05-10 16:47:36 -0700

    sign out for the day

--- HairSalon.Tests/ControllerTests/HomeControllerTests.cs
+++ HairSalon.Tests/ControllerTests/HomeControllerTests.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HairSalon.Solution.Models;
+using HairSalon.Models;
 
-namespace HairSalon.Solution.Controllers
+namespace HairSalon.Tests.Controllers
 {
     public class HomeController : Controller
     {
