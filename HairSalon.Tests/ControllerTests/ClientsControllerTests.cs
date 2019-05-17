7d093dd46cf50ef65684ebc6f791f943c6b2ee81
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-05-17 16:16:06 -0700

    burnout

---
+++ HairSalon.Tests/ControllerTests/ClientsControllerTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Tests.Controllers
+{
+    public class ClientController : Controller
+    {
+
+        // [TestClass]
+        // public class ClientControllerTest
+        // {
+        //     // [TestMethod]
+        //     // public void Method_Action_Result()
+        //     // {
+        //     //
+        //     // }
+        // }
+    }
+}
